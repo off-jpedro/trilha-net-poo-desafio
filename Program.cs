08fd395ba94b46aff4384fb51e4bc43b64848ae8
off-jpedro <jpedroribeirodasilva@hotmail.com>
2023-12-21 10:45:50 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ Console.WriteLine("Smartphone Nokia");
 Smartphone nokia = new Nokia("123456789", "Modelo Test", "00000" , 32); 
 nokia.ReceberLigacao();
 nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
 
 Console.WriteLine("------------------------------------------------------");
 
@@ -12,3 +13,4 @@ Console.WriteLine("Smartphone Iphone");
 Smartphone iphone = new Iphone("987654321", "Modelo Test", "111111", 64);
 iphone.ReceberLigacao();
 iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
